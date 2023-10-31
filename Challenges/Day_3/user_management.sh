7bcb3e727b324c74f6f44252ce19a54d53b326fa
muhamadkamran674 <93035913+muhamadkamran674@users.noreply.github.com>
2023-10-31 22:41:30 +0500

    Update user_management.sh
    Such a Tough lab

--- Challenges/Day_3/user_management.sh
+++ Challenges/Day_3/user_management.sh
@@ -9,39 +9,44 @@ function display_usage {
     echo "  -r, --reset      Reset password for an existing user account."
     echo "  -l, --list       List all user accounts on the system."
     echo "  -h, --help       Display this help and exit."
+    echo "    Additional Features:"
+    echo "   -i, --info  : Display user details"
+    echo "   -m, --modify: Modify user properties"
 }
 
-# Function to create a new user account
 function create_user {
-    read -p "Enter the new username: " username
+     read -p "Enter the new username: " username
 
     # Check if the username already exists
     if id "$username" &>/dev/null; then
-        echo "Error: The username '$username' already exists. Please choose a different username."
+        echo "Username $username already exists. Please choose a different username."
+        exit 1
+        
     else
-        # Prompt for password (Note: You might want to use 'read -s' to hide the password input)
-        read -p "Enter the password for $username: " password
+    read -s -p "Enter the password for $username: " password
 
-        # Create the user account
-        useradd -m -p "$password" "$username"
-        echo "User account '$username' created successfully."
+    # Create the user with the provided username and password
+    useradd -m "$username" #To create a user with a home directory and set a password, you would use them like this: -m -p
+
+    echo "$username:$password" | chpasswd
+
+    echo "User $username created successfully."
+    
     fi
 }
 
-# Function to delete an existing user account
 function delete_user {
-    read -p "Enter the username to delete: " username
+read -p "Enter the username to delete: " username
 
     # Check if the username exists
     if id "$username" &>/dev/null; then
-        userdel -r "$username"  # -r flag removes the user's home directory
-        echo "User account '$username' deleted successfully."
+        userdel -r "$username"  # Delete the user and their home directory
+        echo "User account '$username' has been deleted."
     else
-        echo "Error: The username '$username' does not exist. Please enter a valid username."
+        echo "User account '$username' does not exist."
     fi
 }
 
-# Function to reset the password for an existing user account
 function reset_password {
     read -p "Enter the username to reset password: " username
 
@@ -58,7 +63,6 @@ function reset_password {
     fi
 }
 
-# Function to list all user accounts on the system
 function list_users {
     echo "User accounts on the system:"
     cat /etc/passwd | awk -F: '{ print "- " $1 " (UID: " $3 ")" }'
@@ -70,6 +74,34 @@ if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     exit 0
 fi
 
+function display_user_details {
+    read -p "Enter the username to display details: " username
+
+    # Check if the username exists
+    if id "$username" &>/dev/null; then
+        echo "User Details for '$username':"
+        finger "$username"
+    else
+        echo "User account '$username' does not exist."
+    fi
+}
+
+# Function to modify user properties
+function modify_user {
+    read -p "Enter the username to modify: " username
+
+    # Check if the username exists
+    if id "$username" &>/dev/null; then
+        # Provide options for modifying user properties (e.g., username, user ID)
+        read -p "Enter the new username: " new_username
+        # Additional modification logic can be added here
+
+        usermod -l "$new_username" "$username"  # Modify the username
+        echo "User account '$username' has been renamed to '$new_username'."
+    else
+        echo "User account '$username' does not exist."
+    fi
+}
 # Command-line argument parsing
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -85,6 +117,12 @@ while [ $# -gt 0 ]; do
         -l|--list)
             list_users
             ;;
+        -i|--info)
+            display_user_details
+            ;;
+        -m|--modify)
+            modify_user
+            ;;  
         *)
             echo "Error: Invalid option '$1'. Use '--help' to see available options."
             exit 1
